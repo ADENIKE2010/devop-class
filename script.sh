ee22672880434553180017a898bea9c3fee592b2
ADENIKE2010 <adenikeodusanya@outlook.com>
2022-12-29 19:32:58 -0600

    github homework done

--- script.sh
+++ script.sh
@@ -4,4 +4,8 @@ git add README.md
 git commit -m "first commit"
 git branch -M main
 git remote add origin 
-git push -u origin maingit 
+git push -u origin maingit
+  if (a -lt 9)
+  echo "that is not correct"
+  exit 1
+  fi
